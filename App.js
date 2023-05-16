db3f1c185faa79dcf352adfd972765fb82ce70ff
eitan Benita <e.benita@lyon.ort.asso.fr>
2023-05-16 16:52:01 +0200

    affichage icone

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 import React, { useEffect, useState } from 'react';
 
 export default function App() {
@@ -8,7 +8,6 @@ export default function App() {
   const [data2, setData2] = useState(null);
   const [error2, setError2] = useState(null);
   const api = '0d3435d470b7ca8074338816578ecb4a';
-  const api2 = 'f801982c8328b2a773c1c2c5fe36672e';
 
   useEffect(() => {
     fetchToday();
@@ -43,7 +42,8 @@ export default function App() {
     return <Text>Chargement en cours...</Text>;
   }
   //console.log(data2.list[0].dt_txt);
-
+  const weatherIcon = data.weather[0].icon;
+  const iconUrl = `http://openweathermap.org/img/w/${weatherIcon}.png`;
 
   return (
     <View style={styles.container}>
@@ -51,11 +51,12 @@ export default function App() {
         <Text style={styles.ville}>Vous êtes à {data.name}</Text>
         <Text>Température Actuelle : {data.main.temp} C°</Text>
         <Text>Le temps : {data.weather[0].main}</Text>
+        <Image source={{ uri: iconUrl }} style={styles.icon} />
       </View>
 
       <View style={styles.container}>
-        <Text>{data2.list[0].dt_txt}</Text>
-        <Text>{data2.list[1].dt_txt}</Text>
+        {/* <Text>{data2.list[0].dt_txt}</Text>
+        <Text>{data2.list[1].dt_txt}</Text> */}
       </View>
       <StatusBar style="auto" />
     </View>
@@ -73,5 +74,8 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     fontSize: 30,
     position: 'relative',
-  }
+  }, icon: {
+    width: 100,
+    height: 100,
+  },
 });
